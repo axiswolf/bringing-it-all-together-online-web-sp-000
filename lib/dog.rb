6fde3582b37cf14e1c55d94da3604f0bee957fa1
Karen V <karenvo@usc.edu>
2019-08-23 08:38:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,8 +70,7 @@ class Dog
     # when two dogs have the same name and different breed, it returns the correct dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
    if !dog.empty?
-     dog_data = dog[0]
-     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+     dog = self.new_from_db(dog[0])
    else
      dog = self.create(name: name, breed: breed)
    end
