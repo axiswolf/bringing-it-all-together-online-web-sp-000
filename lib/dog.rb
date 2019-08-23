def5de48bf7800f4e9b103bad42c948c4c9b9ea0
Karen V <karenvo@usc.edu>
2019-08-23 08:16:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,8 +49,8 @@ class Dog
     dog = Dog.new(name, breed)
     dog.save
     dog
+    binding.pry
   end
-binding.pry
   def self.new_from_db(row)
     # creates an instance with corresponding attribute values
     them_dogs = self.new
